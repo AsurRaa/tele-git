b0864b8c96ab000543d58041f31e5185238294c0
LyhourChhen <ukamilove66@gmail.com>
2020-10-11 13:10:12 +0700

    add color cosole and config

--- index.js
+++ index.js
@@ -3,29 +3,33 @@
 // date: 11/10/2020
 // descriptive: cli-script or module to alert to telegram-group.
 
-const { Telegraf } = require('telegraf')
-const { argv } = require('yargs')
-
+const { Telegraf } = require("telegraf");
+const { argv } = require("yargs");
+const config = require("./telegit.json");
+const chalk = require("chalk");
 // constant
-const telegram_to = '-1001417035739'
-const telegram_token = '1399320990:AAHv4eFAO1qoGLTYLXnKJeUdSZbY2HE2TDE'
-let message = ''
+const telegram_to = config.telegram_to;
+const telegram_token = config.telegram_token;
+let message = "";
 
 if (argv.message) {
-  console.log('arge', argv.message)
-  message = argv.message
+  console.log(
+    chalk.blue.bold("your message:\n-------------\n"),
+    chalk.red(`| ${argv.message}`)
+  );
+  message = argv.message;
 } else {
   // null
-  throw "message can't be null."
+  throw "message can't be null.";
 }
 
 // process & function
 const sendToGroup = () => {
-  bot.telegram.sendMessage(telegram_to, message)
-}
-const bot = new Telegraf(telegram_token)
-sendToGroup()
-bot.launch()
+  bot.telegram.sendMessage(telegram_to, message);
+};
+const bot = new Telegraf(telegram_token);
+sendToGroup();
+bot.launch();
 setTimeout(() => {
-  process.exit()
-}, 1000)
+  process.exit();
+}, 1000);
